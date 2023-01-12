7e60a5f196f86c5a17501cbf67172b2623c7c47b
rg81893 <rguillorit@gmail.com>
2023-01-12 10:51:52 +0100

    Point 5

---
+++ Assets/Scripts/Bomb.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bomb : MonoBehaviour
+{
+    [SerializeField] private int _damage = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
